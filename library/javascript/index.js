3ce3fccf30399b08b1de950a3780e4cac95178e0
StuffzEZ <158745985+StuffzEZ@users.noreply.github.com>
2024-08-08 14:20:49 +0100

    Improvements! YAY!
    #nomoreproblems

--- library/javascript/index.js
+++ library/javascript/index.js
@@ -1,1 +1,11 @@
-window.location('https://olliegamer59.github.io/home')
+if (window.location == "https://olliegamer59.github.io") {
+    location.replace('https://olliegamer59.github.io/home')
+}
+
+if (window.location == "https://olliegamer59.github.io/index") {
+    location.replace('https://olliegamer59.github.io/home')
+}
+
+if (window.location == "https://olliegamer59.github.io/index.html") {
+    location.replace('https://olliegamer59.github.io/home')
+}
